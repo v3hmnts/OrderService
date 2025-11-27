e995e14fe041b7373edbd0472580d129558e8218
Ave <avemortalis@gmail.com>
2025-11-27 10:26:22 +0300

    initial dependencies

---
+++ src/test/java/orderService/TestOrderServiceApplication.java
@@ -0,0 +1,11 @@
+package orderService;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(OrderServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
