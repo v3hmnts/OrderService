0e9c4751336ffee20a3ec547b96aac47b611951c
Ave <avemortalis@gmail.com>
2025-11-27 11:55:51 +0300

    add liquidbase changesets

---
+++ src/main/resources/db/changelog/changeset/002-create-indexes.sql
@@ -0,0 +1,13 @@
+--liquibase formatted sql
+
+--changeset vhmnts:001-add-items-indexes
+CREATE INDEX idx_items_name ON items(name);
+CREATE INDEX idx_items_price ON items(price);
+
+--changeset vhmnts:002-add-orders-indexes
+CREATE INDEX idx_orders_user_id ON orders(user_id);
+
+--changeset vhmnts:003-add-order-items-indexes
+CREATE INDEX idx_order_items_order_id ON order_items(order_id);
+CREATE INDEX idx_order_items_item_id ON order_items(item_id);
+CREATE INDEX idx_order_items_order_item ON order_items(order_id, item_id);
