7e1f1efd9aec54ef333f92953f6c620883b25fd4
Ave <avemortalis@gmail.com>
2025-12-01 22:25:13 +0300

    implemented UserServiceClient

---
+++ src/main/java/orderService/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package orderService.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(Long userId) {
+        super(String.format("User with id %s not found",userId));
+    }
+}
--- src/main/java/orderService/service/impl/UserServiceRestClientImpl.java
+++ src/main/java/orderService/service/impl/UserServiceRestClientImpl.java
@@ -1,8 +1,12 @@
 package orderService.service.impl;
 
 import orderService.dto.UserDto;
+import orderService.exception.ServiceUnavailableException;
+import orderService.exception.UserNotFoundException;
 import orderService.service.UserServiceClient;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -20,7 +24,7 @@ public class UserServiceRestClientImpl implements UserServiceClient {
     public UserDto findUserById(Long userId, JwtAuthenticationToken jwtAuthenticationToken) {
         Long userIdFromToken = extractUserIdFromJwtToken(jwtAuthenticationToken);
         if(!userIdFromToken.equals(userId)){
-            throw new RuntimeException();
+            throw new AccessDeniedException("User can only access their own information");
         }
         String USER_BY_ID_RELATIVE_API_URI = "api/v1/users/{userId}";
         return userServiceRestClient
@@ -28,8 +32,17 @@ public class UserServiceRestClientImpl implements UserServiceClient {
                 .uri(USER_BY_ID_RELATIVE_API_URI,userId)
                 .header("Authorization","Bearer "+jwtAuthenticationToken.getToken().getTokenValue())
                 .retrieve()
+                .onStatus((httpStatusCode -> httpStatusCode.isSameCodeAs(HttpStatusCode.valueOf(403))), (request, response) -> {
+                    throw new AccessDeniedException("User does not have permission to access this resource");
+                })
+                .onStatus((httpStatusCode -> httpStatusCode.isSameCodeAs(HttpStatusCode.valueOf(404))), (request, response) -> {
+                    throw new UserNotFoundException(userId);
+                })
                 .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
-                    throw new RuntimeException(response.getStatusCode().toString());
+                    throw new BadRequestException("Invalid request: " + response.getStatusText());
+                })
+                .onStatus(HttpStatusCode::is5xxServerError, (request, response) -> {
+                    throw new ServiceUnavailableException("User service is temporarily unavailable. Please try again later.");
                 })
                 .body(UserDto.class);
 
