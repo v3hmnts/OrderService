2dcbcb9463bc6e30a17a03db7bacfa94029e3a23
Ave <avemortalis@gmail.com>
2025-12-01 22:11:57 +0300

    small fixes

--- src/main/java/orderService/configuration/UserServiceClientConfiguration.java
+++ src/main/java/orderService/configuration/UserServiceClientConfiguration.java
@@ -1,6 +1,7 @@
 package orderService.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
@@ -10,6 +11,7 @@ public class UserServiceClientConfiguration {
     @Value("${user-service.uri}")
     private String userServiceUri;
 
+    @Bean
     public RestClient userServiceRestClient(){
         return RestClient.create(userServiceUri);
     }
