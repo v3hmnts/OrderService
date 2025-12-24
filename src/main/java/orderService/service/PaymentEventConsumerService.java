4f451f171c3e19cce328c127b74fb913c0caefac
Ave <avemortalis@gmail.com>
2025-12-24 18:15:58 +0300

    minor fixes

--- src/main/java/orderService/service/PaymentEventConsumerService.java
+++ src/main/java/orderService/service/PaymentEventConsumerService.java
@@ -5,6 +5,8 @@ import orderService.entity.Order;
 import orderService.entity.enums.OrderStatus;
 import orderService.exception.OrderNotFoundException;
 import orderService.repository.OrderRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -14,7 +16,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class PaymentEventConsumerService {
 
-    private OrderRepository orderRepository;
+    private final Logger log = LoggerFactory.getLogger(PaymentEventConsumerService.class);
+    private final OrderRepository orderRepository;
 
     public PaymentEventConsumerService(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
@@ -30,11 +33,17 @@ public class PaymentEventConsumerService {
         Order orderToEdit = orderRepository.findById(paymentEvent.getOrderId()).orElseThrow(()->new OrderNotFoundException(paymentEvent.getOrderId()));
         switch (paymentEvent.getStatus()){
             case FAILED -> {
+                log.info("Payment for order with id {} failed", orderToEdit.getId());
                 orderToEdit.setOrderStatus(OrderStatus.CANCELED);
                 break;
             }
             case SUCCESS -> {
-                orderToEdit.setOrderStatus(OrderStatus.PAYED);
+                if((paymentEvent.getPaymentAmount().compareTo(orderToEdit.getTotalPrice()) == 0)){
+                    log.info("Payment for order with id {} succeed", orderToEdit.getId());
+                    orderToEdit.setOrderStatus(OrderStatus.PAYED);
+                    break;
+                }
+                log.info("Payment for order with id {} succeed, but payment amount {} lower than totalPrice {}", orderToEdit.getId(),paymentEvent.getPaymentAmount(),orderToEdit.getTotalPrice());
                 break;
             }
         }
