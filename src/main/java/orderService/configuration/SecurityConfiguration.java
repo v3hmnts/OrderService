274c31220a6d65e407d827b50bd81dbaec9c9e3d
Ave <avemortalis@gmail.com>
2026-01-11 23:25:45 +0300

    k8s changes

--- src/main/java/orderService/configuration/SecurityConfiguration.java
+++ src/main/java/orderService/configuration/SecurityConfiguration.java
@@ -31,6 +31,8 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/actuator/health/liveness").permitAll()
+                        .requestMatchers("/actuator/health/readiness").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt
